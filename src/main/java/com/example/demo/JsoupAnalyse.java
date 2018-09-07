7052230b820a4588e2c2cdecee4a538f5ccc649f
滕米 <502698663@qq.com>
2018-09-07 10:28:19 +0800

    第一次采集

--- src/main/java/com/example/demo/JsoupAnalyse.java
+++ src/main/java/com/example/demo/JsoupAnalyse.java
@@ -6,25 +6,38 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class JsoupAnalyse {
-	private static String useContentRex = "";
+	private static String useContentRex = "#houselist-mod-new .list-item";
 	
 	public static void analyseHtml(String content) {
 		Document document = Jsoup.parse(content);
 		
 		Elements elements = document.select(useContentRex);//获取有用的信息,html信息列表
 		for (Element element : elements) {
-			
-//		获取herf
-			System.out.println("子引用："+element.attr("href"));
+			System.out.println("**************************************");
+			Elements housedetails = element.select(".house-details .house-title a");
+			//获取子引用
+			System.out.println("子引用："+housedetails.get(0).attr("href"));
+			//获取标题
+			System.out.println("标题："+housedetails.get(0).attr("title"));
 //			详细信息
-			Elements details = document.select(".details-item span");
+			Elements details = element.select(".details-item span");
 			if (details != null && details.size() > 0) {
 				
-				System.out.println("房屋类型："+elements.get(0).text());
-				System.out.println("房屋面积："+elements.get(1).text());
-				System.out.println("楼层："+elements.get(2).text());
-				System.out.println("建筑年度："+elements.get(2).text());
+				System.out.println("房屋类型："+details.get(0).text());
+				System.out.println("房屋面积："+details.get(1).text());
+				System.out.println("楼层："+details.get(2).text());
+				System.out.println("建筑年度："+details.get(3).text());
+				String address = details.get(5).text();
+				String[] addstr = address.split(" ");
+				System.out.println("小区名称："+addstr[0]);
+				System.out.println("地址："+addstr[1]);
+				String[] zoons = addstr[1].split("-");
+				System.out.println("区域块："+zoons[0]);
 			}
+//			价格
+			Elements prices = element.select(".pro-price span");
+			System.out.println("总价："+prices.get(0).text());
+			System.out.println("单价："+prices.get(1).text());
 		}
 		
 		
