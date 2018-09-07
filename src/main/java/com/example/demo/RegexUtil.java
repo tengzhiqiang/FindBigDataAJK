c4596549893bd10ecd5fb125730b6ee02a91d7e5
滕米 <502698663@qq.com>
2018-09-07 20:29:33 +0800

    正则表达式

---
+++ src/main/java/com/example/demo/RegexUtil.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexUtil {
+	
+	public static void main(String[] args) {
+		System.out.println(getInteger("1s1s1sfd汉中"));
+	}
+
+	/**
+	 * 返回数字，没有小数或者符号
+	 * @param content
+	 * @return
+	 */
+	public static String getInteger(String content) {
+		Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher isNum = pattern.matcher(content);
+		if (isNum.find()) {
+			return isNum.
+		}
+		
+		return null;
+	}
+}
