ddbd97250b5cf8ec1932136f90ddffb77d74fd10
滕米 <502698663@qq.com>
2018-09-26 17:23:09 +0800

    提取66代理IP

--- src/main/java/com/example/demo/FindBigDataAjkApplication.java
+++ src/main/java/com/example/demo/FindBigDataAjkApplication.java
@@ -9,4 +9,15 @@ public class FindBigDataAjkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FindBigDataAjkApplication.class, args);
 	}
+	
+	
+	public static void name() {
+		content = content.substring(content.indexOf("<body>")+6,content.lastIndexOf("<br>"));
+		System.out.println(content);
+		String[] ipv4 = content.split("<br>");
+		System.out.println("**********************分割线*******************");
+		for (String string : ipv4) {
+			System.out.println(string);
+		}
+	}
 }
